cafd938681a7d823663e5d730b0a89598f3f17a4
Owen Lieberman <51335761+Lieb12@users.noreply.github.com>
2025-05-01 12:58:09 -0400

    initial

---
+++ main.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+const static int north[3] = {12, 31, 40};
+const static int south[3] = {21, 30, 49};
+const static int east[2]  = {2, 9};
+const static int west[2]  = {8, 16};
+
+char grid[50][256];
+
+typedef struct {
+    int x;
+    int y;
+    char dir;
+} Car;
+
+void PlaceCar(Car *car, int x, int y) {
+    car->x = x;
+    car->y = y;
+    car->dir = ' ';
+}
+
+int inArray(const int *arr, int size, int val) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == val) return 1;
+    }
+    return 0;
+}
+
+void GetDirection(Car *car) {
+    int options[4][2];
+    int count = 0;
+
+    if (inArray(north, 3, car->x) && car->y > 0)
+        options[count][0] = car->x, options[count++][1] = car->y - 1;
+
+    if (inArray(south, 3, car->x) && car->y < 17)
+        options[count][0] = car->x, options[count++][1] = car->y + 1;
+
+    if (inArray(east, 2, car->y) && car->x < 50)
+        options[count][0] = car->x + 1, options[count++][1] = car->y;
+
+    if (inArray(west, 2, car->y) && car->x > 12)
+        options[count][0] = car->x - 1, options[count++][1] = car->y;
+
+    if (count == 1) {
+        car->x = options[0][0];
+        car->y = options[0][1];
+    } else if (count > 1) {
+        int pick = rand() % count;
+        car->x = options[pick][0];
+        car->y = options[pick][1];
+    }
+}
+
+void PrintGrid(Car cars[], int carCount) {
+    FILE *f = fopen("grid.txt", "r");
+    if (!f) return;
+
+    char temp[50][256];
+    int lines = 0;
+    while (fgets(temp[lines], 256, f)) lines++;
+    fclose(f);
+    printf("\n");
+
+    for (int i = 0; i < carCount; i++) {
+        int x = cars[i].x;
+        int y = cars[i].y;
+        if (y >= 0 && y < lines && x >= 0 && x < strlen(temp[y]))
+            temp[y][x] = 'O';
+    }
+
+    system("cls");
+
+    for (int i = 0; i < lines; i++) {
+        printf("%s", temp[i]);
+    }
+}
+
+int main() {
+    srand(time(NULL));
+
+    Car car1, car2, car3, car4, car5;
+    PlaceCar(&car1, 12, 2);
+    PlaceCar(&car2, 12, 30);
+    PlaceCar(&car3, 31, 3);
+    PlaceCar(&car4, 40, 10);
+    PlaceCar(&car5, 21, 9);
+
+    Car cars[] = {car1, car2, car3, car4, car5};
+
+    while (1) {
+        for (int i = 0; i < 5; i++) {
+            GetDirection(&cars[i]);
+        }
+        PrintGrid(cars, 5);
+        usleep(500000);
+    }
+
+    return 0;
+}
